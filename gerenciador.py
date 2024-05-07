0ad19967819a297c39ca69e6274aa213b4cc1beb
unknown <lsome@live.com>
2024-05-07 01:06:53 -0300

    Acrescentando Atualizar tarefas

--- gerenciador.py
+++ gerenciador.py
@@ -7,6 +7,23 @@ def adicionar_tarefa(tarefas, nome_tarefa):
   print(f"Tarefa {nome_tarefa} foi adicionada com sucesso!")
   return
 
+def ver_tarefas(tarefas):
+  print("\nLista de tarefas:")
+  for indice, tarefa in enumerate(tarefas, start=1):
+    status = "✓" if tarefa["completada"] else " "
+    nome_tarefa = tarefa["tarefa"]
+    print(f"{indice}. [{status}] {nome_tarefa}")
+  return
+
+def atualizar_nome_tarefa(tarefas, indice_tarefa, novo_nome_tarefa):
+  indice_tarefa_ajustado = int(indice_tarefa) - 1
+  if indice_tarefa_ajustado >= 0 and indice_tarefa_ajustado < len(tarefas):
+    tarefas[indice_tarefa_ajustado]["tarefa"] = novo_nome_tarefa
+    print(f"Tarefa {indice_tarefa} atualizada para {novo_nome_tarefa}")
+  else:
+    print("Índice de tarefas inválido.")
+  return
+
 tarefas = []
 while True:
   print("\nMenu do Gerenciador de Lista de tarefas:")
@@ -22,6 +39,13 @@ while True:
   if escolha == "1":
     nome_tarefa = input("Digite o nome da tarefa que deseja adicionar: ")
     adicionar_tarefa(tarefas, nome_tarefa)
+  elif escolha == "2":
+    ver_tarefas(tarefas)
+  elif escolha == "3":
+    ver_tarefas(tarefas)
+    indice_tarefa = input("Digite o número da tarefa que deseja atualizar: ")
+    novo_nome = input("Digite o novo nome da tarefa: ")
+    atualizar_nome_tarefa(tarefas, indice_tarefa, novo_nome)
   elif escolha == "6":
     break
 
